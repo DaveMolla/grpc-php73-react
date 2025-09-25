2c93dcff920e112a8f9fff992fed7af5c8e98b90
davemolla <dawitmolla98@gmail.com>
2025-09-25 16:46:11 +0300

    some customization

--- backend/worker.php
+++ backend/worker.php
@@ -1,15 +1,17 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use App\Service\PingService;
-use Ping\PingServiceInterface;
 use Spiral\GRPC\Server;
-use Spiral\GRPC\Invoker;
-use Spiral\RoadRunner\Worker;
+use Spiral\GRPC\Invoker;     
+use Spiral\RoadRunner\Worker; 
 use Spiral\Goridge\StreamRelay;
+use Ping\PingServiceInterface;
+use App\Service\PingService;
 
 $relay  = new StreamRelay(STDIN, STDOUT);
-$worker = new Worker($relay);
+$worker = new Worker($relay); 
 $server = new Server(new Invoker(), ['debug' => false]);
+
 $server->registerService(PingServiceInterface::class, new PingService());
+
 $server->serve($worker);
